70e61fb932670e21a461be14ac8d4e2d27ae8687
estellar_boyz <tayalone@gmail.com>
2020-05-06 00:33:28 +0700

    :sparkles: [ADD] added basic pg config

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const { Pool } = require('pg')
+
 const {
   redisHost,
   redisPort,
@@ -7,3 +13,26 @@ const {
   pgPassword,
   pgPort
 } = require('./keys')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const pgClient = new Pool({
+  user: pgUser,
+  host: pgHost,
+  database: pgDatabase,
+  password: pgPassword,
+  port: pgPort
+})
+
+pgClient.on('error', () => {
+  console.error('Lost PG connection !!!')
+})
+
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch((err) => {
+    console.error(err)
+  })
