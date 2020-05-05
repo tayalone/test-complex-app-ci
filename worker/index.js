71a576f92d0bc5bb426fbbd3b34bf8a6793e2362
estellar_boyz <tayalone@gmail.com>
2020-05-06 00:01:26 +0700

    :sparkles: [ADD] เพิ่ม fibonacci calculate function

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,19 @@
+const redis = require('redis')
+
 const { redisHost, redisPort } = require('./keys')
+
+const redisClient = redis.redisClient({
+  host: redisHost,
+  port: redisPort,
+  retry_strategy: () => 1000 // ถ้า redis พังจะต่อใหม่ทุกๆ 1000 ms
+})
+
+const sub = redisClient.duplicate()
+
+const fib = (index) => {
+  if (index < 2) {
+    return 1
+  } else {
+    return fib(index - 1) + fib(index - 2)
+  }
+}
