7f47c031296024f7d4d747bfd1d06b028e6d7d9a
estellar_boyz <tayalone@gmail.com>
2020-05-06 13:22:55 +0700

    :heavy_plus_sign: add axios

---
+++ client/src/Fib.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Fib extends Component {
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  }
+  componentDidMount() {}
+  async fetchValues() {}
+  async fetchIndexes() {}
+  render() {
+    return <div></div>
+  }
+}
